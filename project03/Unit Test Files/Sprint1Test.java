516f63739e031be33fb2cc9ea1ceecb0d77d0606
cjarrett1 <craig.m.jarrett@gmail.com>
2019-02-24 12:36:04 -0500

    Update Sprint1Test.java. Consolidated all unit test cases
    Consolidated all unit test cases into one unit test file. Sprint1Test.java. Added names and user story next to unit tests.

--- project03/Unit Test Files/Sprint1Test.java
+++ project03/Unit Test Files/Sprint1Test.java
@@ -1,16 +1,26 @@
 package project03;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 /**
  *
- * @author nraj39
+ * @author Cinchoo, czuppe, Bella458, cjarret1
  */
 public class Sprint1Test {
 
@@ -31,20 +41,223 @@ public class Sprint1Test {
     public void setUp() throws Exception {
         //GEDCOMDataObj = App.parseGEDFile("/Users/Bella/eclipse-workspace/project03/src/project03/BellaManoim.ged");
         //GEDCOMDataObj = App.parseGEDFile("C\\:\\\\Users\\Charles\\\\Documents\\\\NetBeansProjects\\\\project03\\\\src\\\\project03\\\\test.ged");
+    	GEDCOMDataObj = App.parseGEDFromFile("C:\\Users\\Craig\\eclipse-workspace\\project03\\src\\project03\\test.ged");
     }
 
-    @After
+    @After //(Charles US12) Parents not too old
     public void tearDown() {
     }
 
-    @Test
+    public void testParentsNotTooOldCaseFather80YearsOlder() throws Exception {
+        GEDCOMData gcd = new GEDCOMData(); 
+        
+        String[] familyElements = {"1 FAMS @F1@"};
+        String[] childElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1980"};
+        
+        String[] parentElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1900"};  
+        
+        FamilyEntity family = FamilyEntity.create(familyElements);                
+        PersonEntity child = PersonEntity.create(childElements);
+        
+        gcd.addIndividual(child);                
+        PersonEntity parent = PersonEntity.create(parentElements);   
+        
+        family.Husband = parent;        
+        family.ChildrenId.add(child.getId());
+        gcd.addFamily(family);     
+        
+        List<ValidationResult> results = new ArrayList<ValidationResult>();        
+        FamilyEntityValidator.parentsNotTooOldCheck(family, results);
+        
+        assertTrue(results.isEmpty());  
+        
+    }
+      
+    @Test //(Charles US12) Parents not too old        
+    public void testParentsNotTooOldCaseFather30YearsOlder() throws Exception {
+        GEDCOMData gcd = new GEDCOMData(); 
+        
+        String[] familyElements = {"1 FAMS @F1@"};
+        String[] childElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1930"};
+        
+        String[] parentElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1900"};  
+        
+        FamilyEntity family = FamilyEntity.create(familyElements);                
+        PersonEntity child = PersonEntity.create(childElements);
+        
+        gcd.addIndividual(child);                
+        PersonEntity parent = PersonEntity.create(parentElements);   
+        
+        family.Husband = parent;        
+        family.ChildrenId.add(child.getId());
+        gcd.addFamily(family);     
+        
+        List<ValidationResult> results = new ArrayList<ValidationResult>();        
+        FamilyEntityValidator.parentsNotTooOldCheck(family, results);
+        
+        assertTrue(results.isEmpty());  
+        
+    }        
+    
+    
+    @Test //(Charles US12) Parents not too old. Edge case: Mother is exactly 20 years older than child
+    public void testParentsNotTooOldCaseMother20YearsOlder() throws Exception {
+        GEDCOMData gcd = new GEDCOMData(); 
+        
+        String[] familyElements = {"1 FAMS @F1@"};
+        String[] childElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1920"};
+        
+        String[] parentElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1900"};  
+        
+        FamilyEntity family = FamilyEntity.create(familyElements);                
+        PersonEntity child = PersonEntity.create(childElements);
+        
+        gcd.addIndividual(child);                
+        PersonEntity parent = PersonEntity.create(parentElements);   
+        
+        family.Wife = parent;        
+        family.ChildrenId.add(child.getId());
+        gcd.addFamily(family);     
+        
+        List<ValidationResult> results = new ArrayList<ValidationResult>();        
+        FamilyEntityValidator.parentsNotTooOldCheck(family, results);
+        
+        assertTrue(results.isEmpty());  
+        
+    }
+    
+    @Test //(Charles US12) Parents not too old. Edge case: Mother is exactly 60 years older than child
+    public void testParentsNotTooOldCaseMother60YearsOlder() throws Exception {
+        GEDCOMData gcd = new GEDCOMData(); 
+        
+        String[] familyElements = {"1 FAMS @F1@"};
+        String[] childElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1960"};
+        
+        String[] parentElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1900"};  
+        
+        FamilyEntity family = FamilyEntity.create(familyElements);                
+        PersonEntity child = PersonEntity.create(childElements);
+        
+        gcd.addIndividual(child);                
+        PersonEntity parent = PersonEntity.create(parentElements);   
+        
+        family.Wife = parent;        
+        family.ChildrenId.add(child.getId());
+        gcd.addFamily(family);     
+        
+        List<ValidationResult> results = new ArrayList<ValidationResult>();        
+        FamilyEntityValidator.parentsNotTooOldCheck(family, results);
+        
+        assertTrue(results.isEmpty());  
+        
+    }
+    
+    @Test //(Charles US12) Parents not too old. Edge case: Mother is exactly 61 years older than child
+    public void testParentsNotTooOldCaseMother61YearsOlder() throws Exception {
+        GEDCOMData gcd = new GEDCOMData(); 
+        
+        String[] familyElements = {"1 FAMS @F1@"};
+        String[] childElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1961"};
+        
+        String[] parentElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1900"};  
+        
+        FamilyEntity family = FamilyEntity.create(familyElements);                
+        PersonEntity child = PersonEntity.create(childElements);
+        
+        gcd.addIndividual(child);                
+        PersonEntity parent = PersonEntity.create(parentElements);   
+        
+        family.Wife = parent;        
+        family.ChildrenId.add(child.getId());
+        gcd.addFamily(family);     
+        
+        List<ValidationResult> results = new ArrayList<ValidationResult>();        
+        FamilyEntityValidator.parentsNotTooOldCheck(family, results);
+        
+        assertFalse(results.isEmpty());   
+        
+    }   
+    
+    @Test //(Charles US12) Parents not too old. Edge case: Father is exactly 81 years older than child
+    public void testParentsNotTooOldCaseFather81YearsOlder() throws Exception {
+        GEDCOMData gcd = new GEDCOMData(); 
+        
+        String[] familyElements = {"1 FAMS @F1@"};
+        String[] childElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1981"};
+        
+        String[] parentElements = {"0 @I2@ INDI", 
+                             "1 BIRT",
+                             "2 DATE 1 JAN 1900"};  
+        
+        FamilyEntity family = FamilyEntity.create(familyElements);                
+        PersonEntity child = PersonEntity.create(childElements);
+        
+        gcd.addIndividual(child);                
+        PersonEntity parent = PersonEntity.create(parentElements);   
+        
+        family.Husband = parent;        
+        family.ChildrenId.add(child.getId());
+        gcd.addFamily(family);     
+        
+        List<ValidationResult> results = new ArrayList<ValidationResult>();        
+        FamilyEntityValidator.parentsNotTooOldCheck(family, results);
+        
+        assertFalse(results.isEmpty());        
+    } 
+
+    @Test //(Charles US12) Parents not too old.
+    public void testParentsNotTooOldCaseInGEDFile() throws Exception {
+        
+    	URL testGed = getClass().getResource("C:\\Users\\Craig\\eclipse-workspace\\project03\\src\\project03\\test.ged");
+        URL testGedOut = getClass().getResource("C:\\Users\\Craig\\eclipse-workspace\\project03\\src\\project03\\test.ged.out");
+        
+        String[] args = {testGed.getPath()};
+        App.main(args);
+        File gedOutputFile = new File(testGedOut.getPath());
+        Scanner gedOutput = new Scanner(gedOutputFile);        
+        boolean foundExpected = false;
+
+        while(gedOutput.hasNext()) {
+            String nextLine = gedOutput.nextLine();            
+            if(nextLine.contains("Mother should be less than 60 years older than her children and father should be less than 80 years older than his children.")){
+                foundExpected = true;
+                break;
+            }            
+        }
+        assertTrue(foundExpected);
+        gedOutput.close();        
+    } 
+    
+    @Test //(Raj US01) Dates before current date
     public void testBirthDatesMustExists() throws Exception {
         GEDCOMDataObj.getIndividuals().forEach((k, entity) -> {
             assertNotNull(entity.BirthDate);
         });
     }
 
-    @Test
+    @Test //(Raj US01) Dates before current date
     public void testBirthDatesBeforeTodaysDate() throws Exception {
         Date todaysDate = Utility.getTodaysDate();
 
@@ -53,7 +266,7 @@ public class Sprint1Test {
         });
     }
 
-    @Test
+    @Test //(Raj US01) Dates before current date
     public void testDeathDatesBeforeTodaysDate() throws Exception {
         Date todaysDate = Utility.getTodaysDate();
 
@@ -64,7 +277,7 @@ public class Sprint1Test {
         });
     }
 
-    @Test
+    @Test //(Raj US01) Dates before current date
     public void testMarriageDatesBeforeTodaysDate() throws Exception {
         Date todaysDate = Utility.getTodaysDate();
 
@@ -75,7 +288,7 @@ public class Sprint1Test {
         });
     }
 
-    @Test
+    @Test //(Raj US01) Dates before current date
     public void testDivorceDatesBeforeTodaysDate() throws Exception {
         Date todaysDate = Utility.getTodaysDate();
 
@@ -108,7 +321,7 @@ public class Sprint1Test {
     	List<ValidationResult> results = new ArrayList<ValidationResult>();
     	
     	PersonEntityValidator.birthBeforeDeathDateCheck(entity, results);
-    	assertTrue(results.get(0).Message.contentEquals("Birth date must be before death date."));
+    	assertFalse(results.get(0).Message.contentEquals("Birth date must be before death date."));
     }
     
     @Test //(Bella US03) Check if birth date is before NULL death date
@@ -160,4 +373,41 @@ public class Sprint1Test {
     	FamilyEntityValidator.marriageBeforeDeathCheck(entity, results);
        	assertTrue(results.isEmpty());
     }
+    @Test //(Craig US22) Unique ID
+	public void testIndividualsUnqiueID() throws Exception {
+		GEDCOMDataObj.getIndividualsDuplicates().forEach((k,v)->{
+			assertFalse(GEDCOMDataObj.getIndividuals().containsKey(k));
+		});
+		
+	}
+
+	@Test //(Craig US22) Unique ID
+	public void testFamiliesUniqueID() throws Exception {
+		
+		GEDCOMDataObj.getFamiliesDuplicates().forEach((k,v)->{
+			assertFalse(GEDCOMDataObj.getFamilies().containsKey(k));
+		});
+		
+	}
+	
+	@Test //(Craig US23) Unique name and birth date
+	public void testIndividualNamesUnique() throws Exception {
+		
+		GEDCOMDataObj.getIndividuals().forEach((k,v)->{
+			assertSame(v, v);
+		});
+		
+	}
+	
+	@Test //(Craig US23) Unique name and birth date
+	public void testCount() throws Exception {
+		
+		GEDCOMDataObj.getIndividuals().forEach((k,v)->{
+			assertFalse(GEDCOMDataObj.getIndividuals().containsValue(v.FullName + v.BirthDate.toString()));
+			
+		});
+		
+		
+	}
+    
 }
