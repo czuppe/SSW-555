8579a9ce31dc72099d7f1a2e01976417144197bc
Bella Manoim <bella458@yahoo.com>
2019-02-24 11:15:26 -0500

    updated us03 and us05

--- project03/Sprint1Test.java
+++ project03/Sprint1Test.java
@@ -86,39 +86,78 @@ public class Sprint1Test {
         });
     }
 
-    @Test //(Bella US03)
-    public void testBirthDatesBeforeDeathDate() throws Exception {
-        GEDCOMDataObj.getFamilies().forEach((k, entity) -> {
-            if (entity.Husband != null && entity.Husband.DeathDate != null && entity.Husband.BirthDate != null) {
-                assertTrue(entity.Husband.BirthDate.before(entity.Husband.DeathDate));
-            }
-            if (entity.Wife != null && entity.Wife.DeathDate != null && entity.Wife.BirthDate != null) {
-                assertTrue(entity.Wife.BirthDate.before(entity.Wife.DeathDate));
-            }
-        });
+      @Test //(Bella US03) test valid birth date is before valid death date
+    public void testValidDatesBeforeDeathDate() throws Exception {
+    	PersonEntity entity = new PersonEntity();
+    	
+    	entity.BirthDate = new Date(2010,3,30);
+    	entity.DeathDate = new Date(2013,2,2);
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.birthBeforeDeathDateCheck(entity, results);
+    	assertTrue(results.isEmpty());
     }
     
-    /* COMMENTING THIS OUT, can someone review? I am trying to ensure the individuals (not just husband and wife) birth dates are before death date.
-    @Test //(Bella US03)
-    public void testChildrenBirthDatesBeforeDeathDate() throws Exception {
-
-        GEDCOMDataObj.Individuals.forEach((k, entity) -> {
-        	if (entity.BirthDate != null && entity.DeathDate != null) {
-        		assertTrue(entity.BirthDate.before(entity.DeathDate));   
-        	}
-        });       
-    } */
     
-    @Test //(Bella US05)
-    public void testMarriageBeforeDeathDate() throws Exception {
-    	GEDCOMDataObj.getFamilies().forEach((k, entity) -> {
-            if (entity.Husband != null && entity.MarriageDate != null && entity.Husband.DeathDate != null) {
-                assertTrue(entity.MarriageDate.before(entity.Husband.DeathDate));
-            }
-            if (entity.Wife != null && entity.MarriageDate != null && entity.Wife.DeathDate != null) {
-                assertTrue(entity.MarriageDate.before(entity.Wife.DeathDate));
-            }
-        });
+    @Test //(Bella US03) Check if invalid birth date is before valid death date
+    public void testInvalidDatesBeforeDeathDate() throws Exception {
+    	PersonEntity entity = new PersonEntity();
+    	
+    	entity.BirthDate = new Date(2014,3,30);
+    	entity.DeathDate = new Date(2010,2,2);
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.birthBeforeDeathDateCheck(entity, results);
+    	assertTrue(results.get(0).Message.contentEquals("Birth date must be before death date."));
     }
     
+    @Test //(Bella US03) Check if birth date is before NULL death date
+    public void testDatesBeforeNullDeathDate() throws Exception {
+    	PersonEntity entity = new PersonEntity();
+    	
+    	entity.BirthDate = new Date(2014,3,30);
+    	entity.DeathDate = null;
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.birthBeforeDeathDateCheck(entity, results);
+    	assertTrue(results.isEmpty());
+    }
+    
+    @Test //(Bella US03) Check if Null birth date is before death date
+    public void testNullBirthDatesBeforeDeathDate() throws Exception {
+    	PersonEntity entity = new PersonEntity();
+    	
+    	entity.BirthDate = null;
+    	entity.DeathDate = new Date(2014,5,1);
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.birthBeforeDeathDateCheck(entity, results);
+    	assertTrue(results.isEmpty());
+    	
+    }
+    
+    @Test //(Bella US03) Check if Null birth date is before NULL death date
+    public void testnullBirthDatesBeforeNullDeathDate() throws Exception {
+    	PersonEntity entity = new PersonEntity();
+    	
+    	entity.BirthDate = null;
+    	entity.DeathDate = null;
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.birthBeforeDeathDateCheck(entity, results);
+    	assertTrue(results.isEmpty());
+    }
+    
+    @Test //(Bella US05) 
+    public void testValidMarriageBeforeDeathDate() throws Exception {
+    	FamilyEntity entity = new FamilyEntity();
+    	entity.Husband = new PersonEntity();
+    	
+    	entity.Husband.DeathDate = new Date(2043,3,30);
+    	entity.MarriageDate = new Date(2000,2,4);
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	FamilyEntityValidator.marriageBeforeDeathCheck(entity, results);
+       	assertTrue(results.isEmpty());
+    }
 }
