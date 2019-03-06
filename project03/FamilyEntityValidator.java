fc8b3094fd1d2efef5e33f087a7c69c11f3bdba0
Craig <craig.m.jarrett@gmail.com>
2019-03-06 00:22:51 -0500

    Added user stories US08 and US09 to FamilityEntityValidator.java

--- project03/FamilyEntityValidator.java
+++ project03/FamilyEntityValidator.java
@@ -17,162 +17,237 @@ import java.util.List;
  */
 public class FamilyEntityValidator {
 
-    private FamilyEntityValidator() {
+	private FamilyEntityValidator() {
 
-    }
+	}
 
-    //US01: Dates before current date
-    //Dates (birth, marriage, divorce, death) should not be after the current date
-    public static void datesBeforeCurrentDateCheck(FamilyEntity entity, List<ValidationResult> results) {
-        if (entity == null || results == null) {
-            return;
-        }
+	// US01: Dates before current date
+	// Dates (birth, marriage, divorce, death) should not be after the current date
+	public static void datesBeforeCurrentDateCheck(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null) {
+			return;
+		}
 
-        Date todaysDate = Utility.getTodaysDate();
-        if (entity.Marriage != null && entity.Marriage.Date != null && entity.Marriage.Date.after(todaysDate)) {
-            results.add(new ValidationResult("Marriage " + Utility.DateToString(entity.Marriage.Date) + " occurs in the future.", entity, "US01"));
-        }
+		Date todaysDate = Utility.getTodaysDate();
+		if (entity.Marriage != null && entity.Marriage.Date != null && entity.Marriage.Date.after(todaysDate)) {
+			results.add(new ValidationResult(
+					"Marriage " + Utility.DateToString(entity.Marriage.Date) + " occurs in the future.", entity,
+					"US01"));
+		}
 
-        if (entity.Divorce != null && entity.Divorce.Date != null && entity.Divorce.Date.after(todaysDate)) {
-            results.add(new ValidationResult("Divorce " + Utility.DateToString(entity.Divorce.Date) + " occurs in the future.", entity, "US01"));
-        }
-    }
+		if (entity.Divorce != null && entity.Divorce.Date != null && entity.Divorce.Date.after(todaysDate)) {
+			results.add(new ValidationResult(
+					"Divorce " + Utility.DateToString(entity.Divorce.Date) + " occurs in the future.", entity, "US01"));
+		}
+	}
 
-    //US02: Birth before marriage
-    //Birth should occur before marriage of an individual
-    public static void birthBeforeMarriageCheck(FamilyEntity entity, List<ValidationResult> results) {
-        if (entity == null || results == null)
-            return;
+	// US02: Birth before marriage
+	// Birth should occur before marriage of an individual
+	public static void birthBeforeMarriageCheck(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null)
+			return;
 
-        if (entity.Marriage == null || entity.Marriage.Date == null) {
-            return;
-        }
+		if (entity.Marriage == null || entity.Marriage.Date == null) {
+			return;
+		}
 
-        if (entity.Husband != null && entity.Husband.BirthDate != null) {
-            if (entity.Marriage.Date.before(entity.Husband.BirthDate)) {
-                results.add(new ValidationResult("Birthday " + Utility.DateToString(entity.Husband.BirthDate) + " should occur before marriage " + Utility.DateToString(entity.Marriage.Date) + ".", entity, "US02"));
-            }
-        }
+		if (entity.Husband != null && entity.Husband.BirthDate != null) {
+			if (entity.Marriage.Date.before(entity.Husband.BirthDate)) {
+				results.add(new ValidationResult("Birthday " + Utility.DateToString(entity.Husband.BirthDate)
+						+ " should occur before marriage " + Utility.DateToString(entity.Marriage.Date) + ".", entity,
+						"US02"));
+			}
+		}
 
-        if (entity.Wife != null && entity.Wife.BirthDate != null) {
-            if (entity.Marriage.Date.before(entity.Wife.BirthDate)) {
-                results.add(new ValidationResult("Birthday " + Utility.DateToString(entity.Wife.BirthDate) + " should occur before marriage " + Utility.DateToString(entity.Marriage.Date) + ".", entity, "US02"));
-            }
-        }
-    }
-   
-    //US04:	Marriage before divorce 
-    public static void marriageBeforeDivorceCheck(FamilyEntity entity, List<ValidationResult> results) {
-        if (entity == null || results == null) {
-            return;
-        }
-        if (entity.Marriage == null || entity.Marriage.Date == null) {
-            return;
-        }
-        if (entity.Divorce == null || entity.Divorce.Date == null) {
-            return;
-        }
-        if (entity.Husband != null && entity.Divorce.Date != null) {
-            if (entity.Marriage.Date.after(entity.Divorce.Date)) {
-                results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date) + " must be before Divorce date " + Utility.DateToString(entity.Divorce.Date) + " .", entity, "US04"));
-            }
-        }
-        if (entity.Wife != null && entity.Divorce.Date != null) {
-            if (entity.Marriage.Date.after(entity.Divorce.Date)) {
-                results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date) + " must be before Divorce date " + Utility.DateToString(entity.Divorce.Date) + " .", entity, "US04"));
-            }
-        }
-    }
+		if (entity.Wife != null && entity.Wife.BirthDate != null) {
+			if (entity.Marriage.Date.before(entity.Wife.BirthDate)) {
+				results.add(new ValidationResult("Birthday " + Utility.DateToString(entity.Wife.BirthDate)
+						+ " should occur before marriage " + Utility.DateToString(entity.Marriage.Date) + ".", entity,
+						"US02"));
+			}
+		}
+	}
 
-    //US05: Marriage before death : Marriage should occur before death of either spouse
-    public static void marriageBeforeDeathCheck(FamilyEntity entity, List<ValidationResult> results) {
-        if (entity == null || results == null)
-            return;
+	// US04: Marriage before divorce
+	public static void marriageBeforeDivorceCheck(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null) {
+			return;
+		}
+		if (entity.Marriage == null || entity.Marriage.Date == null) {
+			return;
+		}
+		if (entity.Divorce == null || entity.Divorce.Date == null) {
+			return;
+		}
+		if (entity.Husband != null && entity.Divorce.Date != null) {
+			if (entity.Marriage.Date.after(entity.Divorce.Date)) {
+				results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date)
+						+ " must be before Divorce date " + Utility.DateToString(entity.Divorce.Date) + " .", entity,
+						"US04"));
+			}
+		}
+		if (entity.Wife != null && entity.Divorce.Date != null) {
+			if (entity.Marriage.Date.after(entity.Divorce.Date)) {
+				results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date)
+						+ " must be before Divorce date " + Utility.DateToString(entity.Divorce.Date) + " .", entity,
+						"US04"));
+			}
+		}
+	}
 
-        if (entity.Marriage == null || entity.Marriage.Date == null) {
-            return;
-        }
-        
-        if (entity.Husband != null && entity.Husband.DeathDate != null) {
-        	if (entity.Marriage.Date.after(entity.Husband.DeathDate)) {
-        		results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date) + " must be before Husband's death date " + Utility.DateToString(entity.Husband.DeathDate) + ".", entity, "US05"));
-        	}
-        }
-        if (entity.Wife != null && entity.Wife.DeathDate != null) {
-        	if (entity.Marriage.Date.after(entity.Wife.DeathDate)) {
+	// US05: Marriage before death : Marriage should occur before death of either
+	// spouse
+	public static void marriageBeforeDeathCheck(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null)
+			return;
 
-        		results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date) + " must be before Wife's death date " + Utility.DateToString(entity.Wife.DeathDate) + ".", entity, "US05"));
-        	}
-        } 
-    }
+		if (entity.Marriage == null || entity.Marriage.Date == null) {
+			return;
+		}
 
-    //US06: Divorce before death
-    public static void divorceBeforeDeathCheck(FamilyEntity entity, List<ValidationResult> results) {
-        if (entity == null || results == null)
-            return;
+		if (entity.Husband != null && entity.Husband.DeathDate != null) {
+			if (entity.Marriage.Date.after(entity.Husband.DeathDate)) {
+				results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date)
+						+ " must be before Husband's death date " + Utility.DateToString(entity.Husband.DeathDate)
+						+ ".", entity, "US05"));
+			}
+		}
+		if (entity.Wife != null && entity.Wife.DeathDate != null) {
+			if (entity.Marriage.Date.after(entity.Wife.DeathDate)) {
 
-        if (entity.Divorce != null && entity.Divorce.Date != null) {
-            //check divorce before death of either spouse (husband/wife)            
-            if (entity.Husband != null && entity.Husband.DeathDate != null && entity.Husband.DeathDate.before(entity.Divorce.Date)) {
-                results.add(new ValidationResult("US06: Divorce date " 
-                        + Utility.DateToString(entity.Divorce.Date)
-                        + " occurs after "
-                        + Utility.DateToString(entity.Husband.DeathDate) 
-                        + " (death of spouse " 
-                        + entity.Husband.getId() 
-                        + ")." , entity, "US06"));
-            }
-            if (entity.Wife != null && entity.Wife.DeathDate != null && entity.Wife.DeathDate.before(entity.Divorce.Date)) {
-                results.add(new ValidationResult("US06: Divorce date " 
-                        + Utility.DateToString(entity.Divorce.Date) 
-                        + " occurs after "
-                        + Utility.DateToString(entity.Wife.DeathDate) 
-                        + " (death of spouse " 
-                        + entity.Wife.getId() 
-                        + ")." , entity, "US06"));
-            }            
-        }
-    }
+				results.add(new ValidationResult("Marriage date " + Utility.DateToString(entity.Marriage.Date)
+						+ " must be before Wife's death date " + Utility.DateToString(entity.Wife.DeathDate) + ".",
+						entity, "US05"));
+			}
+		}
+	}
 
-    //US12: Parents not too old
-    // Mother should be less than 60 years older than her children and father should be less than 80 years older than his children
-    public static void parentsNotTooOldCheck(FamilyEntity entity, List<ValidationResult> results) {
-        if (entity == null || results == null)
-            return;
+	// US06: Divorce before death
+	public static void divorceBeforeDeathCheck(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null)
+			return;
 
-        if (entity.ChildrenId != null) {
-            //LocalDate husbandBirthdate = entity.Husband != null ? entity.Husband.BirthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate() : null;
-            LocalDate husbandBirthdate = entity.Husband != null ? Utility.ToLocalDate(entity.Husband.BirthDate) : null;            
-            LocalDate wifeBirthdate = entity.Wife != null ?Utility.ToLocalDate(entity.Wife.BirthDate) : null;
+		if (entity.Divorce != null && entity.Divorce.Date != null) {
+			// check divorce before death of either spouse (husband/wife)
+			if (entity.Husband != null && entity.Husband.DeathDate != null
+					&& entity.Husband.DeathDate.before(entity.Divorce.Date)) {
+				results.add(new ValidationResult("US06: Divorce date " + Utility.DateToString(entity.Divorce.Date)
+						+ " occurs after " + Utility.DateToString(entity.Husband.DeathDate) + " (death of spouse "
+						+ entity.Husband.getId() + ").", entity, "US06"));
+			}
+			if (entity.Wife != null && entity.Wife.DeathDate != null
+					&& entity.Wife.DeathDate.before(entity.Divorce.Date)) {
+				results.add(new ValidationResult("US06: Divorce date " + Utility.DateToString(entity.Divorce.Date)
+						+ " occurs after " + Utility.DateToString(entity.Wife.DeathDate) + " (death of spouse "
+						+ entity.Wife.getId() + ").", entity, "US06"));
+			}
+		}
+	}
 
-            for (String childId : entity.ChildrenId) {
-                PersonEntity child = entity.getGEDCOMData().getIndividuals().get(childId);
-                if (child != null) {
-                LocalDate childBirthdate = child.BirthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
- 
-                if (husbandBirthdate != null && Utility.YearsBetween(husbandBirthdate, childBirthdate) > 80) {
-                    results.add(new ValidationResult("US12: Father " + entity.HusbandId + " is " + Utility.YearsBetween(husbandBirthdate, childBirthdate) 
-                            + " years older than his child " + child.getId(), entity, "US12"));
-                }
-                
-                if (wifeBirthdate != null && Utility.YearsBetween(wifeBirthdate, childBirthdate) > 60) {
-                    results.add(new ValidationResult("US12: Mother " + entity.WifeId + " is " + Utility.YearsBetween(wifeBirthdate, childBirthdate) 
-                            + " years older than his child " + child.getId(), entity, "US12"));
-                }
-                }                
-            }
-        }
+	// US12: Parents not too old
+	// Mother should be less than 60 years older than her children and father should
+	// be less than 80 years older than his children
+	public static void parentsNotTooOldCheck(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null)
+			return;
 
-    //US15: There should be fewer than 15 siblings in a family
-    public static void fewerThan15SiblingsCheck(FamilyEntity entity, List<ValidationResult> results) {
-        if (entity == null || results == null)
-            return;
-        if (entity.Children.size() > 15) {
-            results.add(new ValidationResult("There should be fewer than 15 siblings in a family.", entity, "US15"));
-        }
-    }
+		if (entity.ChildrenId != null) {
+			// LocalDate husbandBirthdate = entity.Husband != null ?
+			// entity.Husband.BirthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+			// : null;
+			LocalDate husbandBirthdate = entity.Husband != null ? Utility.ToLocalDate(entity.Husband.BirthDate) : null;
+			LocalDate wifeBirthdate = entity.Wife != null ? Utility.ToLocalDate(entity.Wife.BirthDate) : null;
+
+			for (String childId : entity.ChildrenId) {
+				PersonEntity child = entity.getGEDCOMData().getIndividuals().get(childId);
+				if (child != null) {
+					LocalDate childBirthdate = child.BirthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+					if (husbandBirthdate != null && Utility.YearsBetween(husbandBirthdate, childBirthdate) > 80) {
+						results.add(new ValidationResult("US12: Father " + entity.HusbandId + " is "
+								+ Utility.YearsBetween(husbandBirthdate, childBirthdate)
+								+ " years older than his child " + child.getId(), entity, "US12"));
+					}
+
+					if (wifeBirthdate != null && Utility.YearsBetween(wifeBirthdate, childBirthdate) > 60) {
+						results.add(new ValidationResult("US12: Mother " + entity.WifeId + " is "
+								+ Utility.YearsBetween(wifeBirthdate, childBirthdate) + " years older than his child "
+								+ child.getId(), entity, "US12"));
+					}
+				}
+
+			}
+		}
+	}
+
+	// US15: There should be fewer than 15 siblings in a family
+	public static void fewerThan15SiblingsCheck(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null)
+			return;
+		if (entity.Children.size() > 15) {
+			results.add(new ValidationResult("There should be fewer than 15 siblings in a family.", entity, "US15"));
+		}
+	}
+
+	// US08: Birth before marriage of parents
+	public static void birthBeforeMarriageParents(FamilyEntity entity, List<ValidationResult> results) {
+		if (entity == null || results == null) {
+			return;
+		}
+		
+		if (entity.Marriage == null || entity.Marriage.Date == null) {
+			return;
+		}
+
+		if (entity.Marriage != null && entity.Marriage.Date != null) {
+			LocalDate marriageDate = entity.MarriageDate != null ? Utility.ToLocalDate(entity.MarriageDate) : null;
+			LocalDate divorceDate = entity.DivorceDate != null ? Utility.ToLocalDate(entity.DivorceDate) : null;
+			PersonEntity childFamID = new PersonEntity();
+
+			for (String childId : childFamID.ChildhoodFamilyIds) {
+				PersonEntity child = entity.getGEDCOMData().getIndividuals().get(childId);
+				if (child != null) {
+					LocalDate childBirthdate = child.BirthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+					if (childBirthdate.isBefore(marriageDate) || childBirthdate.isAfter(divorceDate.plusMonths(9))) {
+						results.add(new ValidationResult("US08: The child's birthday " + childBirthdate
+								+ " should occur after marriage or not after divorce" + ".", entity, "US08"));
+
+					}
+				}
+			}
+		}
+	}
+	
+	// US09: Birth before death of parents
+		public static void birthBeforeDeathParents(FamilyEntity entity, List<ValidationResult> results) {
+			PersonEntity person = new PersonEntity();
+			
+			if (entity == null || results == null) {
+				return;
+			}
+			
+			if (entity.Marriage == null || entity.Marriage.Date == null) {
+				return;
+			}
+
+			if (entity.Marriage != null && entity.Marriage.Date != null) {
+				LocalDate wifeDeathdate = entity.Wife != null ? Utility.ToLocalDate(entity.Wife.DeathDate) : null;
+				LocalDate husbandDeathdate = entity.Husband != null ? Utility.ToLocalDate(entity.Husband.BirthDate) : null;
+				
+
+				for (String childId : person.ChildhoodFamilyIds) {
+					PersonEntity child = entity.getGEDCOMData().getIndividuals().get(childId);
+					if (child != null) {
+						LocalDate childBirthdate = child.BirthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+						if (childBirthdate.isBefore(wifeDeathdate) || childBirthdate.isBefore(husbandDeathdate.plusMonths(9))) {
+							results.add(new ValidationResult("US09: The child's birthday " + childBirthdate
+									+ " should occur before the mother's death or before 9 months after father's death" + ".", entity, "US09"));
+
+						}
+					}
+				}
+			}
+		}
 }
-   
-
-    
-
