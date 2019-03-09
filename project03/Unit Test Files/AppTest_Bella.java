ef0b2e5607c05c1d547635e8e54eb488596da5d5
Bella Manoim <bella458@yahoo.com>
2019-03-09 00:34:37 -0500

    US04 and US07 added Sprint 2

--- project03/Unit Test Files/AppTest_Bella.java
+++ project03/Unit Test Files/AppTest_Bella.java
@@ -8,6 +8,9 @@ package project03;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -40,7 +43,6 @@ public class AppTest {
 
     @Before
     public void setUp() throws Exception {
-        GEDCOMDataObj = App.parseGEDFile("/Users/Bella/eclipse-workspace/project03/src/project03/BellaManoim.ged");
     }
 
     @After
@@ -70,7 +72,7 @@ public class AppTest {
     	List<ValidationResult> results = new ArrayList<ValidationResult>();
     	
     	PersonEntityValidator.birthBeforeDeathDateCheck(entity, results);
-    	assertTrue(results.get(0).Message.contentEquals("Birth date must be before death date."));
+    	assertTrue(results.get(0).Message.contains("Birth"));
     }
     
     @Test //(Bella US03) Check if birth date is before NULL death date
@@ -122,5 +124,88 @@ public class AppTest {
     	FamilyEntityValidator.marriageBeforeDeathCheck(entity, results);
        	assertTrue(results.isEmpty());
     }
+    
+    // /*
+    @Test //US07: Less then 150 years old (birth and death date)
+    public void testlessthan150YearsOldCheck() throws Exception {
+        PersonEntity entity = new PersonEntity();
+        //date constructor requires me to subtract year from 1900
+        entity.BirthDate = new Date(-50,3,30);
+    	entity.DeathDate = new Date(119,3,30);
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.lessthan150YearsOldCheck(entity, results);
+    	assertTrue(results.get(0).Message.contains("Age"));
+    }
+	// */
+    
+    
+    @Test //US07: Test Less then 150 years old (no death date)
+    public void testlessthan150YearsOldNoDeathDateCheck() throws Exception {
+        PersonEntity entity = new PersonEntity();
 
+        //date constructor requires me to subtract year from 1900
+        entity.BirthDate = new Date(-50,5,4);
+    	entity.DeathDate = null;
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.lessthan150YearsOldCheck(entity, results);
+    	assertTrue(results.get(0).Message.contains("Age"));
+    }
+	
+    
+    // /*
+    @Test //US07: Test Less then 150 years old (valid)
+    public void testvalidlessthan150YearsOldCheck() throws Exception {
+        PersonEntity entity = new PersonEntity();
+
+        //date constructor requires me to subtract year from 1900
+        entity.BirthDate = new Date(50,1,6);
+    	entity.DeathDate = new Date(100,2,3);
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	PersonEntityValidator.lessthan150YearsOldCheck(entity, results);
+    	assertTrue(results.isEmpty());
+    }
+	// */
+	 @Test //US04:	Marriage before divorce (Valid)
+    public void testvalidmarriageBeforeDivorceCheck() throws Exception {
+    
+    	FamilyEntity entity = new FamilyEntity();
+    	entity.Husband = new PersonEntity();
+    	entity.Wife = new PersonEntity();
+    	
+    	entity.Divorce = new FactEntity();
+    	entity.Marriage = new FactEntity();
+    	
+    	//date constructor requires me to subtract year from 1900
+    	entity.Marriage.Date = entity.MarriageDate = new Date(10,3,30);
+    	entity.Divorce.Date = entity.DivorceDate = new Date(20,2,4);
+    	List<ValidationResult> results = new ArrayList<ValidationResult>();
+    	
+    	FamilyEntityValidator.marriageBeforeDivorceCheck(entity, results);
+       	assertTrue(results.isEmpty());
+       	//System.out.println(results); //should print message
+    }
+	 
+	 @Test //US04:	Marriage before divorce (invalid)
+	   public void testinvalidmarriageBeforeDivorceCheck() throws Exception {
+	    
+	    FamilyEntity entity = new FamilyEntity();
+	    entity.Husband = new PersonEntity();
+	    entity.Wife = new PersonEntity();
+	    
+	   //date constructor requires me to subtract year from 1900
+	    entity.Marriage = new FactEntity();
+	    entity.Marriage.Date = entity.MarriageDate = new Date(99,3,30);
+	    
+	    entity.Divorce = new FactEntity();
+	    entity.Divorce.Date = entity.DivorceDate = new Date(97,2,4);
+	    
+	    List<ValidationResult> results = new ArrayList<ValidationResult>();
+	    FamilyEntityValidator.marriageBeforeDivorceCheck(entity, results);
+	    	
+	    assertTrue(results.get(0).Message.contains("Marriage"));
+	    //System.out.println(results); //should print empty list
+	 }
 }
