91df47cee0712f8dccb5fdd397a53a1a09d053c2
Bella Manoim <bella458@yahoo.com>
2019-03-27 20:43:43 -0400

    US30 and US31 Added (Sprint 3)

---
+++ project03/Input Files/App.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project03;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.text.ParseException;
+
+/**
+ *
+ * @author nraj39
+ */
+public class App {
+
+    static HashMap<String, String> tags = new HashMap<String, String>();
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException, FileNotFoundException, ParseException {
+        if (args == null || args.length == 0 || Utility.isNullOrBlank(args[0])) {
+            System.out.println("Missing GED file.");
+            return;
+        }
+        String gedFilePath = args[0];
+
+        File f = new File(gedFilePath);
+        if (!f.exists()) {
+            System.out.println("'" + gedFilePath + "' file not exists.");
+            return;
+        }
+
+        System.out.println("Loading '" + gedFilePath + "' file...");
+
+        initialize();
+
+        //Parse input file
+        GEDCOMData GEDCOMDataObj = parseGEDFromFile(gedFilePath);
+
+        //Print results
+        print(gedFilePath, GEDCOMDataObj);
+    }
+
+    private static void initialize() {
+        tags.put("INDI", "0");
+        tags.put("NAME", "1");
+        tags.put("SEX", "1");
+        tags.put("BIRT", "1");
+        tags.put("DEAT", "1");
+        tags.put("FAMC", "1");
+        tags.put("FAMS", "1");
+        tags.put("FAM", "0");
+        tags.put("MARR", "1");
+        tags.put("HUSB", "1");
+        tags.put("WIFE", "1");
+        tags.put("CHIL", "1");
+        tags.put("DIV", "1");
+        tags.put("DATE", "2");
+        tags.put("HEAD", "0");
+        tags.put("TSLR", "0");
+        tags.put("NOTE", "0");
+    }
+
+
+    public static GEDCOMData parseGEDFromURI(URI gedFileURI) throws FileNotFoundException, IOException, ParseException {
+        return parseGEDFromFile(new File(gedFileURI).getPath());
+    }
+
+    public static GEDCOMData parseGEDFromFile(String gedFilePath) throws FileNotFoundException, IOException, ParseException {
+        String gedcomFileContent = Utility.readFileAsString(gedFilePath);
+        return parseGEDFromText(gedcomFileContent);
+    }
+
+    public static GEDCOMData parseGEDFromText(String gedcomFileContent) throws ParseException {
+
+        GEDCOMData GEDCOMDataObj = new GEDCOMData();
+        String[] records = gedcomFileContent.replaceAll("0 @", "\u0646@").split("\u0646");
+
+        for (String record : records) {
+            if (record.startsWith("@") && !record.startsWith("@SUBM@ SUBM")) {
+                String[] elements = record.split("\n");
+
+                String recordType = elements[0].replaceAll(" ", "").split("@")[2];
+
+                if (recordType.equals("FAM")) {
+                    GEDCOMDataObj.addFamily(FamilyEntity.create(elements));
+                } else if (recordType.equals("INDI")) {
+                    GEDCOMDataObj.addIndividual(PersonEntity.create(elements));
+                } else if (recordType.equals("NOTE")) {
+
+                }
+            }
+        }
+        
+        GEDCOMDataObj.loadReferenceEntitiesToFamily();
+
+        return GEDCOMDataObj;
+    }
+
+    private static void print(String gedFilePath, GEDCOMData GEDCOMDataObj) throws IOException {
+        String gedOutFilePath = gedFilePath + ".out";
+        FileWriter outfw = new FileWriter(gedOutFilePath);
+        PrintWriter writer = new PrintWriter(outfw);
+
+        //Validate entities
+        List<ValidationResult> results = new ArrayList();
+        GEDCOMDataObj.Validate(results);
+
+        try {
+            String msg = GEDCOMDataObj.toPersonsText();
+            System.out.println();
+            System.out.println("Individuals:");
+            System.out.println();
+            System.out.println(msg);
+            writer.println(msg);
+            System.out.println();
+
+            msg = GEDCOMDataObj.toFamiliesText();
+            System.out.println("Families:");
+            System.out.println();
+            System.out.println(msg);
+            writer.println(msg);
+            System.out.println();
+            
+            msg = GEDCOMDataObj.listLivingMarried();
+            System.out.println("Living Married Individuals:");
+            System.out.println();
+            System.out.println(msg);
+            writer.println(msg);
+            System.out.println();
+            
+            msg = GEDCOMDataObj.listLivingSingle();
+            System.out.println("Living Single Individuals:");
+            System.out.println();
+            System.out.println(msg);
+            writer.println(msg);
+            System.out.println();
+
+            printValidationResults(writer, results);
+        } finally {
+            //Close the input stream
+            outfw.close();
+        }
+    }
+
+    private static void printValidationResults(PrintWriter writer, List<ValidationResult> results) throws IOException {
+        if (results != null && !results.isEmpty()) {
+            for (ValidationResult result : results) {
+                System.out.println(result.toString());
+                writer.println(result.toString());
+            }
+        } else {
+            String noResultMsg = "No errors found.";
+            System.out.println(noResultMsg);
+            writer.println(noResultMsg);
+        }
+    }
+}
---
+++ project03/Input Files/GEDCOMData.java
@@ -0,0 +1,269 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project03;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author nraj39
+ */
+public class GEDCOMData {
+
+    private Map<String, PersonEntity> Individuals;
+    private Map<String, FamilyEntity> Families;
+
+    private Map<String, PersonEntity> IndividualsDuplicates;
+    private Map<String, FamilyEntity> FamiliesDuplicates;
+
+    public Map<String, PersonEntity> getIndividualsDuplicates() {
+        return IndividualsDuplicates;
+    }
+
+    public Map<String, FamilyEntity> getFamiliesDuplicates() {
+        return FamiliesDuplicates;
+    }
+
+    public Map<String, PersonEntity> getIndividuals() {
+        return Individuals;
+    }
+	
+    public void setIndividuals(Map<String, PersonEntity> individuals){
+        this.Individuals = individuals;
+    }
+
+    public Map<String, FamilyEntity> getFamilies() {
+        return Families;
+    }
+
+    public GEDCOMData() {
+        Individuals = new HashMap<String, PersonEntity>();
+        Families = new HashMap<String, FamilyEntity>();
+
+        IndividualsDuplicates = new HashMap<String, PersonEntity>();
+        FamiliesDuplicates = new HashMap<String, FamilyEntity>();
+    }
+
+    public void addFamily(FamilyEntity entity) {
+        if (entity == null) {
+            return;
+        }
+
+        entity.setGEDCOMData(this);
+        if (Families.containsKey(entity.getId())) {
+            FamiliesDuplicates.put(entity.getId(), entity);
+        }
+        Families.put(entity.getId(), entity);
+    }
+
+    public void addIndividual(PersonEntity entity) {
+        if (entity == null) {
+            return;
+        }
+
+        entity.setGEDCOMData(this);
+        if (Individuals.containsKey(entity.getId())) {
+            IndividualsDuplicates.put(entity.getId(), entity);
+        }
+        Individuals.put(entity.getId(), entity);
+    }
+
+    public void Validate(List<ValidationResult> results) {
+        if (results == null) {
+            return;
+        }
+
+        //GEDCOM Validations
+        GEDCOMDataValidator.maleLastNameCheck(this, results);
+        GEDCOMDataValidator.uniqueIDsCheck(this, results);
+        GEDCOMDataValidator.uniqueNameAndBirthDateCheck(this, results);
+
+        //Individual validations
+        Individuals.forEach((k, entity) -> {
+            entity.validate(results);
+        });
+
+        //Familty validations
+        Families.forEach((k, entity) -> {
+            entity.validate(results);
+        });
+    }
+
+    public void loadReferenceEntitiesToFamily() {
+        Families.forEach((k, family) -> {
+            family.Husband = Individuals.get(family.HusbandId);
+            family.Wife = Individuals.get(family.WifeId);
+
+            for (String childId : family.ChildrenId) {
+                PersonEntity child = Individuals.get(childId);
+                if (child != null) {
+                    family.Children.add(child);
+                }
+            }
+        });
+
+        Individuals.forEach((k, individual) -> {
+            Map<String, IEntity> map = new HashMap<String, IEntity>();
+            Map<String, FamilyEntity> x = Families.entrySet().stream().filter(fe -> fe.getValue().Husband != null && fe.getValue().Husband.getId().equals(k)).collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+
+            Iterator i = x.entrySet().iterator();
+            while (i.hasNext()) {
+                Map.Entry<String, FamilyEntity> entry = (Map.Entry) i.next();
+                map.put(entry.getKey(), IEntity.class.cast(entry.getValue()));
+            }
+
+            if (individual.Families == null) {
+                Map<String, FamilyEntity> y = Families.entrySet().stream().filter(fe -> fe.getValue().Wife != null && fe.getValue().Wife.getId().equals(k)).collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+
+                Iterator j = y.entrySet().iterator();
+                while (j.hasNext()) {
+                    Map.Entry<String, FamilyEntity> entry = (Map.Entry) j.next();
+                    map.put(entry.getKey(), IEntity.class.cast(entry.getValue()));
+                }
+            }
+            individual.Families = new ArrayList(map.values());
+        });
+    }
+
+    @Override
+
+    public String toString() {
+        return null;
+    }
+
+    public String toFamiliesText() {
+        StringBuilder msg = new StringBuilder();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        msg.append("ID, Married, Divorced, Husband ID, Husband Name, Wife ID, Wife Name, Children\n");
+        Families.forEach((k, entity) -> {
+            StringBuilder childMsg = new StringBuilder();
+            if (entity.Children != null) {
+                for (PersonEntity ce : entity.Children) {
+                    if (childMsg.length() == 0) {
+                        childMsg.append(ce.getId());
+                    } else {
+                        childMsg.append(", ").append(ce.getId());
+                    }
+                }
+            }
+            msg.append(entity.getId() + ", "
+                    + (entity.MarriageDate != null ? format.format(entity.MarriageDate) : "NA") + ", "
+                    + (entity.DivorceDate != null ? format.format(entity.DivorceDate) : "NA") + ", "
+                    + (entity.HusbandId != null ? entity.HusbandId : "NA") + ", "
+                    + (entity.Husband != null ? entity.Husband.FullName : "NA") + ", "
+                    + (entity.WifeId != null ? entity.WifeId : "NA") + ", "
+                    + (entity.Wife != null ? entity.Wife.FullName : "NA") + ", "
+                    + (childMsg.length() == 0 ? "NA" : "{" + childMsg.toString() + "}") + "\n"
+            );
+        });
+        return msg.toString();
+    }
+    
+    public String toPersonsText(Collection<PersonEntity> persons)
+    {
+        StringBuilder msg = new StringBuilder();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        msg.append("ID, Name, Gender, Birthday, Age, Alive, Death, Child, Spouse\n");
+
+        persons.forEach(entity -> {
+            StringBuilder childMsg = new StringBuilder();
+            entity.Families.forEach(fe -> {
+                if (fe != null && fe.Children != null && !fe.Children.isEmpty()) {
+                    fe.Children.forEach(c -> {
+                        if (childMsg.length() == 0) {
+                            childMsg.append(c.getId());
+                        } else {
+                            childMsg.append(", ").append(c.getId());
+                        }
+                    });
+                }
+            });
+
+            StringBuilder spouseMsg = new StringBuilder();
+            entity.Families.forEach(fe -> {
+                if (fe != null) {
+                    if (fe.Husband != null && fe.Husband.getId().equals(entity.getId()) && fe.Wife != null) {
+                        if (spouseMsg.length() == 0) {
+                            spouseMsg.append(fe.Wife.getId());
+                        } else {
+                            spouseMsg.append(", ").append(fe.Wife.getId());
+                        }
+                    } else if (fe.Wife != null && fe.Wife.getId().equals(entity.getId()) && fe.Husband != null) {
+                        if (spouseMsg.length() == 0) {
+                            spouseMsg.append(fe.Husband.getId());
+                        } else {
+                            spouseMsg.append(", ").append(fe.Husband.getId());
+                        }
+                    }
+                }
+            });
+
+            msg.append(entity.getId() + ", " + entity.FullName + ", " + entity.Gender + ", "
+                    + format.format(entity.BirthDate) + ", " + entity.Age + ", " + (entity.DeathDate == null ? true : false) + ", "
+                    + (entity.DeathDate != null ? format.format(entity.DeathDate) : "NA") + ", "
+                    + (childMsg.length() == 0 ? "NA" : "{" + childMsg.toString() + "}") + ", "
+                    + (spouseMsg.length() == 0 ? "NA" : "{" + spouseMsg.toString() + "}") + "\n");
+        });
+
+        return msg.toString();    	
+    }
+
+    public String toPersonsText() {
+        return toPersonsText(Individuals.values());
+    }
+    
+    //US30 (bella) List Living Married Individuals
+    public String listLivingMarried()
+    {
+    	 List<PersonEntity> livingMarriedPersons = new ArrayList<PersonEntity>();
+    	 
+    	 Families.forEach((s, entity) -> {
+	    	 LocalDate marriageDate = entity.MarriageDate != null ? Utility.ToLocalDate(entity.MarriageDate) : null; 
+	         LocalDate divorceDate = entity.DivorceDate != null ? Utility.ToLocalDate(entity.DivorceDate) : null; 
+	
+	         LocalDate husbandDeathdate = entity.Husband != null ? Utility.ToLocalDate(entity.Husband.DeathDate) : null;            
+	         LocalDate wifeDeathdate = entity.Wife != null ? Utility.ToLocalDate(entity.Wife.DeathDate) : null;
+	         
+	         //check if wife is alive and married
+	         if (wifeDeathdate == null & marriageDate != null & divorceDate == null) {
+	             livingMarriedPersons.add(entity.Wife);   
+	         }
+	         
+	         //check if husband is alive and married
+	         if (husbandDeathdate == null & marriageDate != null & divorceDate == null) {
+	        	 livingMarriedPersons.add(entity.Husband);
+	         }
+    	 });
+    	 return toPersonsText(livingMarriedPersons);
+    }	
+    
+    //US31 (bella) List Living Single Individuals
+    public String listLivingSingle()
+    {
+    	 List<PersonEntity> livingSinglePersons = new ArrayList<PersonEntity>();
+    	 
+	     Individuals.forEach((s, entity) -> {
+	    	 
+	    	 if (entity.DeathDate == null ) {
+	    		 boolean single[] = {true};
+	    		 entity.Families.forEach(fe -> {
+	    			 if (fe.DivorceDate == null)
+	    				 single[0] = false;
+	    		 });
+	    		 if (single[0]) {
+	    			 livingSinglePersons.add(entity);
+	    		 }
+	    	 }
+	         
+    	 });
+    	 return toPersonsText(livingSinglePersons);
+    }
+}
